dd1dac03464c6834fcfb065005afc8d2c6de8d25
Pranay Mangal <mangal.pranay@gmail.com>
2025-06-18 18:22:47 -0400

    img path changed

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <div 
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: 'url("/public/Main_image.webp")'
+          backgroundImage: 'url("/Main_image.webp")'
         }}
       />
       <div className="absolute inset-0 bg-hero-pattern backdrop-blur-xs" />
@@ -19,7 +19,7 @@ function App() {
         <header className="p-6 md:p-8">
           <div className="flex items-center gap-3">
             <div className="bg-white p-3 h-[100px]">
-              <img src="/public/Logo.avif" alt="ArtCreativity Logo" className="w-full h-full object-contain" />
+              <img src="/Logo.avif" alt="ArtCreativity Logo" className="w-full h-full object-contain" />
             </div>
             {/* <h1 className="text-xl md:text-2xl font-bold text-white">ArtCreativity</h1> */}
           </div>
